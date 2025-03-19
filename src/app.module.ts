ba36781a35490753efd273caa9ed76abac3dffb6
Reaganz-Wat <reaganwatmon6@gmail.com>
2025-03-19 09:29:35 +0300

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    // TypeOrmModule.forRoot(
+    //   {
+    //     type: 'postgres',
+    //     host: 'localhost',
+    //     port: 5432,
+    //     username: 'postgres',
+    //     password: 'yourpassword',
+    //     database: 'yourdatabase',
+    //     autoLoadEntities: true,
+    //     synchronize: true, // Disable in production
+    //   }
+    // )
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
